2d80950fac58a0ef23c77b59cacf8482901816ba
Nakamori <64877188+NaokoNakamori@users.noreply.github.com>
2021-11-23 21:27:21 +0900

    新たに覚えた用語を追加

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import "./styles.css";
-console.log("TEST");
 
 document.getElementById("app").innerHTML = `
 <h1>学習記録</h1>
 <p>ここに学んだことを記録していきます。</p>
 <h2>2021.11.27</h2>
+
+<p>導入</p>
 <ul>
 <li>CodeSandboxを使ってみる</li>
 <li>GitHubと連携する</li>
+</ul>
+
+<p>新たに覚えた用語</p>
+<ul>
+<li>仮想DOM</li>
+<li>ECMAScript</li>
+<li>モジュールハンドラー</li>
+<li>トランスバイラ</li>
+<li>SPA</li>
 </ul>
 `;
